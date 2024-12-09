d8ca8afad99e6bfa661d8827040dceb7a20f5248
RenoirTan <renoirtan2005@gmail.com>
2024-12-09 23:54:34 +0800

    poetry: full runner

--- src/xremapmanager/_data.py
+++
@@ -1,4 +0,0 @@
-VERSION = "0.1.0"
-PREFIX_DIR = "/usr"
-BIN_DIR = "/usr/bin"
-DATA_DIR = "/usr/share"
--- src/xremapmanager/runner.py
+++ src/xremapmanager/runner.py
@@ -1,2 +1,12 @@
+import shutil
+import subprocess
+
+from xremapmanager._data import CONFIG_FP, DEFAULT_CONFIG_FP
+from xremapmanager.config import XMgrConfig
+
+
 def main():
-    pass
+    if not CONFIG_FP.exists():
+        shutil.copy2(DEFAULT_CONFIG_FP, CONFIG_FP)
+    config = XMgrConfig.json_loadfp(CONFIG_FP)
+    subprocess.run([config.command.path, config.layout])
