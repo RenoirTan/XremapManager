99190d4d15353093d0d6e3f40cff5aa1ab3bc025
RenoirTan <renoirtan2005@gmail.com>
2024-12-10 11:08:42 +0800

    pyqt6: test the tutorial

--- src/xremapmanager/manage.py
+++ src/xremapmanager/manage.py
@@ -1,56 +1,27 @@
-import argparse
-import multiprocessing
-import time
-import webbrowser
+import sys
 
-from flask import Flask, render_template, request
-import waitress
-
-from xremapmanager._data import CONFIG_FP
-from xremapmanager.config import XMgrConfig
+from PySide6 import QtCore, QtWidgets, QtGui
 
 
-def create_app() -> Flask:
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="test",
-    )
-    
-    @app.route("/", methods=["GET", "POST"])
-    def index():
-        config = XMgrConfig.json_loadfp(CONFIG_FP)
-        return render_template("index.html", xrmpcfg=config)
-    
-    return app
-
-
-# Workaround. Delays the request for flask to start
-def open_browser(url: str, delay_ms: int = 500):
-    time.sleep(delay_ms / 1000)
-    webbrowser.open(url)
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+        self.button = QtWidgets.QPushButton("Click me!")
+        
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
 
 
 def main():
-    arg_parser = argparse.ArgumentParser(
-        prog="xrmp-manage",
-        description="Manage Xremap"
-    )
-    arg_parser.add_argument(
-        "-H",
-        "--host",
-        default="127.0.0.1",
-        help="Host to bind to"
-    )
-    arg_parser.add_argument(
-        "-P",
-        "--port",
-        default=8000,
-        type=int,
-        help="Port to bind to"
-    )
-    args = arg_parser.parse_args()
-    app = create_app()
-    p = multiprocessing.Process(target=open_browser, args=(f"http://{args.host}:{args.port}/",))
-    p.start()
-    waitress.serve(app, host=args.host, port=args.port)
-    p.join()
+    app = QtWidgets.QApplication([])
+    widget = MyWidget()
+    widget.resize(800, 800)
+    widget.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
